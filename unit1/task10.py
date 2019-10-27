04c8ed4bb070b4d40876e93d01e097b8788eed92
Hollow-user <51712285+Hollow-user@users.noreply.github.com>
2019-10-27 22:35:54 +1000

    Update task10.py

--- unit1/task10.py
+++ unit1/task10.py
@@ -1,1 +1,53 @@
-"""Напишите программу, которая получает на вход три целых числа, по одному числу в строке, и выводит нав три строки сначала максимальное, потом минимальное, после чего оставшееся число.На ввод могут подаваться и повторяющиеся числа.Sample Input 1:8214Sample Output 1:1428Sample Input 2:232321Sample Output 2:232123"""a = int(input())b = int(input())c = int(input())max = amin = aif b > max:    max = b    if c > max: max = celse:    if c > max: max = cif b < min:    min = b    if c < min: min = celse:    if c < min: min = cx = (a + b + c) - min - maxprint(max)print(min)print(x)
+
+"""
+Напишите программу, которая получает на вход три целых числа, по одному числу в строке, и выводит на
+в три строки сначала максимальное, потом минимальное, после чего оставшееся число.
+
+На ввод могут подаваться и повторяющиеся числа.
+
+Sample Input 1:
+
+8
+2
+14
+Sample Output 1:
+
+14
+2
+8
+Sample Input 2:
+
+23
+23
+21
+Sample Output 2:
+
+23
+21
+23
+"""
+
+a = int(input())
+b = int(input())
+c = int(input())
+
+max = a
+min = a
+
+if b > max:
+    max = b
+    if c > max: max = c
+else:
+    if c > max: max = c
+
+if b < min:
+    min = b
+    if c < min: min = c
+else:
+    if c < min: min = c
+
+x = (a + b + c) - min - max
+
+print(max)
+print(min)
+print(x)
